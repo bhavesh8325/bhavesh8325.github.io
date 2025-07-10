28d1818feb6dfd05e7e562b1b9bfbb395bb59c1c
bhavesh8325 <bhaveshgadiraju7@gmail.com>
2025-07-10 16:59:15 -0400

    Update JSreview.js

--- JSreview.js
+++ JSreview.js
@@ -5,7 +5,7 @@ alert(`This page was created in the STEM PREP ${course} by ${myName}`);
 function output()
 {
   let Name = document.getElementById("Name").value;
-  let School = document.getElemtById("School").value;
+  let School = document.getElementById("School").value;
   let Grade = docuemnt.getElementById("Grade").value;
 
 var output="Name: <b>" + Name + "</b><br>";

b41cacf00ae0dd61e0660342f130684ca87d5c2c
bhavesh8325 <bhaveshgadiraju7@gmail.com>
2025-07-10 16:53:33 -0400

    Update JSreview.js

--- JSreview.js
+++ JSreview.js
@@ -1,3 +1,15 @@
 var course ="Technology";
 let myName ="Bhavesh";
 console.log(`This page was created in the STEM PREP ${course} by ${myName}`);
+alert(`This page was created in the STEM PREP ${course} by ${myName}`);
+function output()
+{
+  let Name = document.getElementById("Name").value;
+  let School = document.getElemtById("School").value;
+  let Grade = docuemnt.getElementById("Grade").value;
+
+var output="Name: <b>" + Name + "</b><br>";
+  output += "Grade: <b>" + Grade + "</b><br>";
+  output += "School: <b>" + School + "</b><br>";
+document.getElementById("output").innerHTML = output;
+}

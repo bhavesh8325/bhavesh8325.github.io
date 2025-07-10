88301ae2fee69270540562a96ed6e6c8dc98e156
bhavesh8325 <bhaveshgadiraju7@gmail.com>
2025-07-10 17:02:53 -0400

    Update JSreview.js

--- JSreview.js
+++ JSreview.js
@@ -6,7 +6,7 @@ function output()
 {
   let Name = document.getElementById("Name").value;
   let School = document.getElementById("School").value;
-  let Grade = docuemnt.getElementById("Grade").value;
+  let Grade = document.getElementById("Grade").value;
 
 var output="Name: <b>" + Name + "</b><br>";
   output += "Grade: <b>" + Grade + "</b><br>";

abe8cc20c5a99718c418246d664648e87c454537
bhavesh8325 <bhaveshgadiraju7@gmail.com>
2025-07-17 16:47:00 -0400

    Update Travelmap.js

--- Travelmap.js
+++ Travelmap.js
@@ -8,3 +8,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+var circlemarker = L.circle(coordinates, {
+color: "blue",
+fillColor: "white"
+fillOpacity: 0.25,
+radius: 50
+}).bindPopup(`<iframe width="280" height="160" src="https://www.youtube.com/embed/zQzLJurMaDk?si=Buf1YGGzQOXhoWc0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`).addTo(map);
